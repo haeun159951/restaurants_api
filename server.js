8b3b2e0fc948aa48776ebde4f25a1754a01081d1
Haeun Kim <haeun159951@gmail.com>
2021-05-20 18:02:49 -0400

    modified

--- server.js
+++ server.js
@@ -4,14 +4,13 @@
  *  No part of this assignment has been copied manually or electronically from any other source
  *  (including web sites) or distributed to other students.
  *
- *  Name: Ha Eun Kim Student ID: 158007187 Date: ________________
- *  Heroku Link: _______________________________________________________________
+ *  Name: Ha Eun Kim Student ID: 158007187 Date: May 23, 2021
+ *  Heroku Link: https://gentle-coast-22304.herokuapp.com
  *
  ********************************************************************************/
 
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
@@ -29,65 +28,69 @@ app.get('/', (req, res) => {
   res.json({ message: 'API Listening' });
 });
 
-//POST
+//POST : 201
 app.post('/api/restaurants', (req, res) => {
   db.addNewRestaurant(req.body)
-    .then((result) => {
-      res.status(201).json({
-        message: result,
-      });
+    .then(() => {
+      res.status(201).json({ message: `added restaurants` });
     })
     .catch((err) => {
       console.log(err);
-      res.status(404).json(err);
+      res.status(404).json({ message: `${err}` });
     });
 });
 
 //GET
 app.get('/api/restaurants', (req, res) => {
   db.getAllRestaurants(req.query.page, req.query.perPage, req.query.borough)
-    .then((rest) => {
-      res.status(201).json(rest);
+    .then((result) => {
+      res.status(200).json(result);
     })
     .catch((err) => {
       console.log(err);
-      res.status(404).json(err);
+      res.status(404).json({ message: `${err}` });
     });
 });
 
-//GET WITH ID
+//GET with ID
 app.get('/api/restaurants/:id', (req, res) => {
   db.getRestaurantById(req.params.id)
-    .then((rest) => {
-      res.status(201).json(rest);
+    .then((result) => {
+      res.status(200).json(result);
     })
     .catch((err) => {
       console.log(err);
-      res.status(404).json(err);
+      res.status(404).json({ message: `${err}` });
     });
 });
 
-//put
+// PUT: update
 app.put('/api/restaurants/:id', (req, res) => {
-  db.updateRestaurantById(req.params.id)
-    .then((rest) => {
-      res.status(201).json(rest);
+  db.updateRestaurantById(req.body, req.params.id)
+    .then((result) => {
+      res.status(200).json({
+        message: result,
+      });
     })
     .catch((err) => {
-      console.log(err);
-      res.status(404).json(err);
+      res.status(400).json({
+        message: `${err}`,
+      });
     });
 });
 
-//delete
+//Delete
 app.delete('/api/restaurants/:id', (req, res) => {
   db.deleteRestaurantById(req.params.id)
-    .then((rest) => {
-      res.status(201).json(rest);
+    .then((result) => {
+      res.status(204).json({
+        message: result,
+      });
     })
     .catch((err) => {
-      console.log(err);
-      res.status(404).json(err);
+      res.status(400).json({
+        message: `${err}`,
+      });
     });
 });
 
